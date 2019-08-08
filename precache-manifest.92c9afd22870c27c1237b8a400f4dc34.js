4122fc3440683d24589a3adb8cbef1a696fa1e59
lvino <lyuniancheng@gamil.com>
2019-08-08 18:07:54 +1000

    Updates

---
+++ precache-manifest.92c9afd22870c27c1237b8a400f4dc34.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b067077dc312065118d7a88642dc992",
+    "url": "/jobBoardDemo/index.html"
+  },
+  {
+    "revision": "a4c696d77e2569485b5c",
+    "url": "/jobBoardDemo/static/css/2.1d6907fe.chunk.css"
+  },
+  {
+    "revision": "53f1685cb16afce49a92",
+    "url": "/jobBoardDemo/static/css/main.0dac1f44.chunk.css"
+  },
+  {
+    "revision": "a4c696d77e2569485b5c",
+    "url": "/jobBoardDemo/static/js/2.48138d05.chunk.js"
+  },
+  {
+    "revision": "53f1685cb16afce49a92",
+    "url": "/jobBoardDemo/static/js/main.e33d9ec9.chunk.js"
+  },
+  {
+    "revision": "1478f1ef4a7ed62cb74f",
+    "url": "/jobBoardDemo/static/js/runtime~main.927d2da3.js"
+  }
+]);
